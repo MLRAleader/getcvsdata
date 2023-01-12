928d51f28693896b546ff0a855816ccd825e7cdf
MLRAleader <mouwenlegan@gmail.com>
2023-01-12 05:04:10 +0100

    creation-function

---
+++ app/app.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+require_once './config/config.php';
+
+$fileWay = FILE_PATH.'sample_1.csv';
+
+/**
+ * Fonction qui récupère le fichier dépuis le dossier.
+ * Cette fonction prend en paramètre un chemin de fichier.
+*/
+function recupFile(string $fileWay){
+
+    if(is_dir($fileWay)){
+        
+    }
+
+}
+
+
+
+?>
--- config/config.php
+++ config/config.php
@@ -5,10 +5,14 @@ $root = dirname(__DIR__);
 
 $root_anti_slashLess=str_replace('\\','/',$root);
 
-var_dump($root_anti_slashLess);
+// var_dump($root_anti_slashLess);
 
 define('CONFI_PATH','config').DIRECTORY_SEPARATOR;
 define('FILE_PATH','files').DIRECTORY_SEPARATOR;
 define('VIEW_FILE','views').DIRECTORY_SEPARATOR;
+
+// $test=VIEW_FILE.'/template.php';
+// var_dump($test);
+
 
 ?>
